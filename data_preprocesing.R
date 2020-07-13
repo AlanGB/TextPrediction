a6962b926be2723b4303361eeaf58f266bdcb91e
Alán García <alangarciabernal@gmail.com>
2020-07-13 16:46:17 -0500

    Added the code for Data Cleaning and Tokenization of the corpora

--- data_preprocesing.R
+++ data_preprocesing.R
@@ -1,14 +1,31 @@
+setwd("~/Alán/CapstoneProject")
+
 library(readr)
 library(quanteda)
+library(tm)
 
 train <- read_lines("./final/en_US/train_US.txt")
 
 # Data Pipeline: ####
+# Profanitry Filtering
 # 0. Tokenize
 # 1. Lowercase
 # 2. Remove stop words
 # 3. Stemming words
 # 4. Unigram-model
+
+train <- iconv(train, to = "UTF-8")
+train <- tolower(train)
+train <- removePunctuation(train)
+train <- removeNumbers(train)
+train <- stripWhitespace(train)
+
+
+con <- url("http://www.bannedwordlist.com/lists/swearWords.txt","r")
+badwords <- readLines(con)
+close(con)
+
+train <- removeWords(train, badwords)
 
 train.tokens <- tokens(train, what = "word",
                        remove_numbers = TRUE, remove_punct = TRUE,
