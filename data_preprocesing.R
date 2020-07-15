aab5639fb7615504e3a25eaa1169611d8879dacc
Alán García <alangarciabernal@gmail.com>
2020-07-14 22:29:54 -0500

    Added the 1-grams, also modified the data pipelien (omitting tokenization and stemming)

--- data_preprocesing.R
+++ data_preprocesing.R
@@ -3,39 +3,46 @@ setwd("~/Alán/CapstoneProject")
 library(readr)
 library(quanteda)
 library(tm)
-
+library(ggplot2)
+library(gridExtra)
 train <- read_lines("./final/en_US/train_US.txt")
 
 # Data Pipeline: ####
-# Profanitry Filtering
+
 # 0. Tokenize
 # 1. Lowercase
-# 2. Remove stop words
-# 3. Stemming words
-# 4. Unigram-model
+# 2. Remove punctuation
+# 3. Remove numbers
+# 4. Tokenization
+#       4.1 1-gram model
+#       4.2 2-gram model
+#       4.3 3-gram model
+# tokenization ####
 
-train <- iconv(train, to = "UTF-8")
-train <- tolower(train)
-train <- removePunctuation(train)
-train <- removeNumbers(train)
-train <- stripWhitespace(train)
-
-
-con <- url("http://www.bannedwordlist.com/lists/swearWords.txt","r")
-badwords <- readLines(con)
-close(con)
-
-train <- removeWords(train, badwords)
-
-start.time <- Sys.time()
 train.tokens <- tokens(train, what = "word",
                        remove_numbers = TRUE, remove_punct = TRUE,
                        remove_symbols = TRUE, split_hyphens = TRUE)
-total.time <- start.time - Sys.time()
 
-start.time <- Sys.time()
 train.tokens <- tokens_tolower(train.tokens)
-train.tokens <- tokens_select(train.tokens, stopwords(), selection = "remove")
-train.tokens <- tokens_wordstem(train.tokens, language = "english")
+#train.tokens <- tokens_select(train.tokens, stopwords(), selection = "remove")
+#train.tokens <- tokens_wordstem(train.tokens, language = "english")
 train.tokens.dfm <- dfm(train.tokens, tolower = FALSE)
-total.time <- start.time - Sys.time()
+
+
+
+train.unigram.freq <- textstat_frequency(train.tokens.dfm, n = 20)
+train.unigram.freq$feature <- factor(train.unigram.freq$feature,
+                                     levels = train.unigram.freq$feature[order(train.unigram.freq$frequency)])
+
+
+
+unigrams.plot <- ggplot(train.unigram.freq, 
+                        aes(x = train.unigram.freq$feature,
+                            y = train.unigram.freq$frequency)) +
+        geom_col() + coord_flip() + labs(x = NULL, y = "Frequency")
+
+textplot_wordcloud(train.tokens.dfm, max_words = 100)
+
+
+wordcloud
+unigrams.plot
